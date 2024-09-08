bd05ac881ab622e00a6db00d1a8fa509be591e61
Hypsilon-debut <bruno92291@gmail.com>
2024-09-07 22:43:10 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,51 @@
+let dados = [
+    {
+        titulo: "Noah - Protagonista.",
+        descricao: "Noah é o protagonista desse manga incrivel chamado sense life,Noah é um garoto pacato que procura por um sentido para sua vida, seus dias cotidianos irão sumir após encontrar Kaleb, um usuário de drogas que tem uma grande recompensa por sua cabeça.",
+        link: "https://crossverse.fandom.com/pt-br/wiki/Noah_(Sense_Life)",
+        tags: "Noah noah protagonista Protagonista"
+    },
+    {
+        titulo: "Kaleb - Sub protagonista.",
+        descricao: "Kaleb é um jovem adulto de 22 anos de idade com cabelo preto com franjas pontiagudas, pupilas em espiral e dentes afiados. Ele normalmente é visto usando um terno sem gravata.",
+        link: "https://sense-life.fandom.com/wiki/Kaleb",
+        tags: "Kaleb kaleb coadjuvante protagonista"
+    },
+    {
+        titulo: "Mortuus - Ajudante.",
+        descricao: "Mortuus é uma agente da CIF treinada para combates contra humanos com tributos ou habilidades especiais. Sendo a melhor nesse quesito de toda organização.",
+        link: "https://crossverse.fandom.com/pt-br/wiki/Mortuus_(Sense_Life)",
+        tags: "mortuus zumbi CIF lutadora"
+    },
+    {
+        titulo: "Delete - Investigadora da CIF",
+        descricao: "Delete tem a dadiva de se apagar da memoria de quem ela quiser por isso pode entrar em contato com qualquer um perguntar o que quiser e sair como se nunca fosse vista assim sendo a investigadora perfeita.",
+        link: "https://crossverse.fandom.com/pt-br/wiki/Delete_(Sense_Life)",
+        tags: "delete erase CIF "
+    },
+    {
+        titulo: "Umbra - Lider da CIF",
+        descricao: "Umbra tem o conhecimento guardado de toda a vida de várias pessoas na sua cabeça. Usando elas para investigações contra outros usuários de dadiva. Sua dadiva permite que ele absorva informações como livros e pessoas e saiba todo seu conteudo.",
+        link: "https://crossverse.fandom.com/pt-br/wiki/Umbra_(Sense_Life)",
+        tags: "umbra lider CIF importante"
+    },
+    {
+        titulo: "Reaver - Mercenario.",
+        descricao: "Reaver era um ladrão famoso, mesmo antes de conseguir seu tributo. O mesmo após consiguir seu atributo de teletransporte vai atrás de kaleb por sua recompensa e recebe sua morte como recompensa.",
+        link: "https://crossverse.fandom.com/pt-br/wiki/Reaver_(Sense_Life)",
+        tags: "rever mercenario muito_foda meu_idolo dadiva"
+    },
+    {
+        titulo: "Lohan - Mercenario.",
+        descricao: "Lohan é um caçador de recompensa que usa de sua dadiva para garantir que seus alvos não fujam, Lohan usa uma foice para matar seus alvos, seu maior prazer é uma luta até a morte, seu tributo funciona de forma com que aqueles que fujam de suas lutas sejam paralizados instantanemanete. O mesmo é acompanhado por Uller seu irmão de caça",
+        link: "https://crossverse.fandom.com/pt-br/wiki/Lohan_(Sense_Life)",
+        tags: "Mercenario lohan dadiva "
+    },
+    {
+        titulo: "Uller - Mercenario.",
+        descricao: "Uller é um mercenario que usa de armas para abater seus inimigos porem tambem pode contar com sua força sobre humana, Uller tem uma mira excepcional e sempre caça com acompanhamento de Lohan seu irmão de caça.",
+        link: "https://crossverse.fandom.com/pt-br/wiki/Uller_(Sense_Life)",
+        tags: "Uller mercenario humano armas"
+    }
+];
+
